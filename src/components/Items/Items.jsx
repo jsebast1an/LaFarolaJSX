6de8fc983172bcaa921f37d67571893c28976804
jsebast1an <juanselopezg05@gmail.com>
2021-11-15 21:16:08 -0300

    Pre entrega 1

--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -7,13 +7,13 @@ function Items({prod}) {
 
     return (
         <>  
-            <Card style={{ width: '22rem' }} className="my-3">
+            <Card style={{ width: '22rem' }} className="my-3 itemsStyle">
                 <Card.Img variant="top" src={prod.img} alt="" width="400" height="300" />
 
                 <Card.Body>
-                    <Card.Title>{prod.nombre}</Card.Title>
-                    <Card.Text>{prod.year}</Card.Text>
-                    <Card.Text>{prod.precio}</Card.Text>
+                    <Card.Title as="h3">{prod.nombre}</Card.Title>
+                    <Card.Text as="h4">{prod.year}</Card.Text>
+                    <Card.Text >Precio: {prod.precio} ARS</Card.Text>
                     <Link to={`/detail/${prod.id}`}>
                         <Button variant="primary">Detalles del producto</Button>
                     </Link>
